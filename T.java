aae6f3b3461f455ab33d724929f4b050d915ee2a
Monteleev <cs0421@uoi.gr>
2021-04-20 17:00:55 +0300

    Made methods static

--- T.java
+++ T.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 class T
 {
-    public ArrayList<Integer> reverse(ArrayList al)
+    public static ArrayList<Integer> reverse(ArrayList al)
     {
 
         for (int i = 0, j = al.size() - 1; i < j; i++)
@@ -12,14 +12,15 @@ class T
 
         return(al);
     }
-    public ArrayList<Integer> join(ArrayList ral, ArrayList ar)
+
+    public static ArrayList<Integer> join(ArrayList ral, ArrayList ar)
     {
         ArrayList<Integer> b = new ArrayList<Integer>(ral);
         b.addAll(ar);
         return(b);
     }
 
-    public ArrayList<ArrayList<Integer>> split(ArrayList<Integer> list,int k)
+    public static ArrayList<ArrayList<Integer>> split(ArrayList<Integer> list,int k)
     {
         ArrayList<Integer> first = new ArrayList<Integer>();
         ArrayList<Integer> second = new ArrayList<Integer>();
@@ -39,21 +40,26 @@ class T
         return(lists);
     }
 
-    public static void main(String[] args) {
-        ArrayList<Integer> a = new ArrayList<Integer>();
-        a.add(3);
-        a.add(5);
-        a.add(4);
-        a.add(1);
-        a.add(2);
-        int k = 4;
-        T temp = new T();
-        ArrayList<ArrayList<Integer>> lists = temp.split(a,k);
+    public static ArrayList<Integer> t(ArrayList a,int k) {
+        ArrayList<ArrayList<Integer>> lists = split(a, k);
         ArrayList<Integer> al = lists.get(0);
         ArrayList<Integer> ar = lists.get(1);
 
-        ArrayList<Integer> ral = temp.reverse(al);
-        ArrayList<Integer> b = temp.join(ral, ar);
+        ArrayList<Integer> ral = reverse(al);
+        ArrayList<Integer> b = join(ral, ar);
+        return (b);
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        a.add(3);
+        a.add(2);
+        a.add(1);
+        a.add(4);
+        a.add(5);
+        int k = 3;
+        ArrayList<Integer> b = t(a,k);
+        System.out.println(a);       
         System.out.print(b);
     }
 }
